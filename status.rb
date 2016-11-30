7d9c528e014057bc317342338b04bdff6030490e
Allison Hurley <allison@hurleyhome.com>
2016-11-29 21:16:32 -0500

    It is pretty much done!

--- status.rb
+++ status.rb
@@ -33,31 +33,42 @@ Dir.chdir(File.expand_path(File.dirname(__FILE__)))
 
 members = IO.read("members.dat").each_line.map {|l| l.split("\t")}
 rfid_lookup = {}
+stud_lookup = {}
 members.each do |name, rfid, studid|
-  rfid_lookup[rfid.rjust(10,"0")] = name
+  rfid = rfid.rjust(10, "0")
+  rfid_lookup[rfid] = name
+  stud_lookup[studid.chomp] = rfid
 end
 
 punches = {}
 
 last_member = "Lightning Robotics"
 status = "Attendance System"
-
+input = ""
+kickoff = Time.new(2017,1,7,10)
+bag = Time.new(2017,2,22)
 log = File.open("rfid.log")
 screen = Screen.new
 screen.poll do |evt, scr|
   scr.text(last_member, 10, 10)
   scr.text(status, 10, 80)
+  scr.text(input, 10, 150)
   now = Time.now
   scr.text(now.strftime("%d %b %Y %l:%M:%S %P"), 10, scr.height - 50, 25)
   scr.text(sprintf("%s-%s", online? ? "online" : "offline", ip_address), 
            10, scr.height - 25, 25)
-
+  if kickoff >= now
+    scr.text(sprintf("kickoff: %d days %d:%02d:%02d", *seconds_to_dhms(kickoff- now)), 10, scr.height - 75, 25)
+  else
+    scr.text(sprintf("bag: %d days %d:%02d:%02d", *seconds_to_dhms(bag- now)), 10, scr.height - 75, 25)
+  end
 begin
   if line = log.gets
     if line.match(/^(.*) (\d+)$/) 
       date,rfid = $1,$2
       date = Time.parse(date) 
       last_member = rfid_lookup[rfid]
+      input = "" 
       if punches[rfid] 
         seconds = (date.to_i - punches[rfid].to_i)
         days,hours,minutes,seconds = seconds_to_dhms(seconds)
@@ -80,6 +91,32 @@ rescue Exception => e
 end
   
   next if evt.nil? 
+
+  if evt.type == :KEYUP 
+    puts "Event #{evt.key.keysym.sym.inspect}"
+    #puts (evt.key.public_methods - Object.new.public_methods).sort.join("\n")
+    STDOUT.flush
+    key = evt.key.keysym.sym
+    ch = key.to_s
+    if ch.match(/KP_(\d)/)
+      last_member = " " 
+      status = " " 
+      input += $1
+    elsif key == :BACKSPACE
+      input = input[0..-2]
+    elsif key == :KP_ENTER
+      if stud_lookup[input] != nil
+        status = stud_lookup[input]
+        last_member = rfid_lookup[status]
+      else 
+        last_member = "ID not found"
+      end 
+      File.open("rfid.log", "a") do |l|
+        l.puts Time.now.to_s + " " + status 
+      end   
+      input = "" 
+    end
+  end
 
   if evt.type == :MOUSEBUTTONUP 
     puts evt.button.x
